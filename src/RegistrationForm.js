cc3e39f79a17fff37c844e205f1d7a61b049afab
Andrey Shevtsov <andrewshevtsov@inbox.ru>
2017-09-21 19:00:46 +0300

    registration form update

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class RegistrationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+    
+    // привязали this дабы не потерять контекст
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('Form is submitted. Email value is', this.state.email);
+  }
+
+  handleEmailChange(event) { // event input'а, в котором содержится value
+    console.log('email was changed', event.target.value);
+
+    // меняем значение в state при вызове handleEmailChange
+    this.setState({email: event.target.value});
+  }
 
   render() {
     return (
-          <div>Form</div>
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              type="text"
+              placeholder="E-mail"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
+            <button>Save</button>
+          </form>
         );
   }
 }
