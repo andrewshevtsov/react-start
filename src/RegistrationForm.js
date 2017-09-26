cb2ec9dfdb2cef709610efdd066e8d533d3cd4e4
Andrey Shevtsov <andrewshevtsov@inbox.ru>
2017-09-26 18:14:08 +0300

    registration form style was added

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './RegistrationForm.css';
 
 class RegistrationForm extends Component {
   constructor(props) {
@@ -32,8 +33,9 @@ class RegistrationForm extends Component {
               placeholder="E-mail"
               value={this.state.email}
               onChange={this.handleEmailChange}
+              className="emailField"
             />
-            <button>Save</button>
+            <button className="submitButton">Save</button>
           </form>
         );
   }
